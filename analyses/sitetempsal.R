8e36ed535a9349cc575cfcb8c103c11ae4d06ed4
Mary <oconnor@zoology.ubc.ca>
2017-04-28 13:33:02 -0700

    temp sal correlation

--- analyses/sitetempsal.R
+++ analyses/sitetempsal.R
@@ -37,50 +37,58 @@ plot(sites2$Sal.a ~ sites2$fetch.jc)
 plot(sites2$Temp.a ~ sites2$dfw)
 
 ### Salinity
-mod1 <- lme(Sal.a ~ Temp.a, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod2 <- lme(Sal.a ~ Temp.a + dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod3 <- lme(Sal.a ~ Temp.a + dfw + fetch.jc, random = ~ 1 | sites2, data = sites2, na.action = na.omit)
+mod1 <- lme(Sal.a ~ I(log(Temp.a)-mean(log(Temp.a))), random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod2 <- lme(Sal.a ~ I(log(Temp.a)-mean(log(Temp.a))) + dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod3 <- lme(Sal.a ~ I(log(Temp.a)-mean(log(Temp.a))) + dfw + fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
 mod4 <- lme(Sal.a ~ 1, random = ~ 1 | Site, data = sites2, na.action = na.omit)
 mod5 <- lme(Sal.a ~ dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod6 <- lme(Sal.a ~ Temp.a*dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod6 <- lme(Sal.a ~ I(log(Temp.a)-mean(log(Temp.a)))*dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
 mod7 <- lme(Sal.a ~ fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
 
 model.sel(mod1, mod2, mod3, mod4, mod5, mod6, mod7)
 
-model.avg(mod5, mod2) -> mod.sal
+model.avg(mod6, mod2) -> mod.sal
 confint(mod.sal)
-
-summary(mod1)
+summary(mod.sal)
 
 
 ### Temperature
-mod1 <- lme(Temp.a ~ Sal.a, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod2 <- lme(Temp.a ~ Sal.a + dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod3 <- lme(Temp.a ~ Sal.a + dfw + fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod4 <- lme(Temp.a ~ 1, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod5 <- lme(Temp.a ~ dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod6 <- lme(Temp.a ~ Sal.a*dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
-mod7 <- lme(Temp.a ~ fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod1 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)), random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod2 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)) + dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod3 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)) + dfw + fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod4 <- lme(log(Temp.a) ~ 1, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod5 <- lme(log(Temp.a) ~ dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod6 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a))*dfw, random = ~ 1 | Site, data = sites2, na.action = na.omit)
+mod7 <- lme(log(Temp.a) ~ fetch.jc, random = ~ 1 | Site, data = sites2, na.action = na.omit)
 
 model.sel(mod1, mod2, mod3, mod4, mod5, mod6, mod7)
 
-model.avg(mod5, mod4, mod1) -> mod.t
-summary(mod.t)
-confint(mod.t)
+summary(mod4)
+confint(mod4)
 
+## how correlated are temp and salinity?
+mod0 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)), random = ~ Sal.a | Site, data = sites2, method = "REML")
+mod1 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)), random = ~ 1 | Site, data = sites2, method = "REML")
+model.sel(mod1, mod0)
 
+mod1 <- lme(log(Temp.a) ~ I(Sal.a - mean(Sal.a)), random = ~ 1 | Site, data = sites2, method = "ML")
+mod2 <- lme(log(Temp.a) ~ 1, random = ~ 1 | Site, data = sites2, method = "ML")
+model.sel(mod1, mod2)
 
-## other code I'm no longer using
-Sal <- abiotic %>%
+summary(mod1) #summary tells us these are correlated, but the relationship is quite weak: -0.007 C / 1 PPT change in salinity.
+
+## to use temp and salinity as predictors of 
+
+temps <- abiotic %>%
+  select(Site, Time.Code, Temp.a, Sal.a) %>%
+  group_by(Site, Time.Code) %>%
+summarise(avgT = mean(Temp.a))
+
+sals <- abiotic %>%
   select(Site, Time.Code, Temp.a, Sal.a) %>%
   group_by(Site, Time.Code) %>%
   summarise(avgS = mean(Sal.a))
 
-plot(temps$avgT ~ temps$Site)
-
-View(sites)
-
-plot(abiotic$Sal.a ~ abiotic$Temp.a)
-
-
-
+sites3 <- merge.data.frame(sites,
+                           temps %>% 
+                           filter(Time.Code == "B"), by.x = "site", by.y = "Site") 
