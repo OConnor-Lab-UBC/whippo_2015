6832776c1550cef31b65dba2c60108ce3d247a62
Mary <oconnor@zoology.ubc.ca>
2018-01-26 15:38:03 +0100

    Mary cleaned up file structure
    removed files, including older manuscript versions, to prepare the
    repository for sharing.

--- Whippo et al CJAFS/table 3 2017/code.R
+++
@@ -1,8 +0,0 @@
-library(MuMIn)
-data<-read.csv("data.csv")
-
-temp.mod<-lm(temp~time+dfw+time*dfw,data=data)
-sal.mod<-lm(sal~time+dfw+time*dfw,data=data)
-sd.mod<-lm(sdens~time+dfw+time*dfw,data=data)
-lai.mod<-lm(lai~time+dfw+time*dfw,data=data)
-el.mod<-lm(epload~time+dfw+time*dfw,data=data)
